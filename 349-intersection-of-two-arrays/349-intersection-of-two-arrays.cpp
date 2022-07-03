61c773e531e2a06298528f58550ccf6379eec262
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-07-03 20:22:12 +0530

    Time: 13 ms (31.61%), Space: 10.7 MB (16.04%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
+    
+      set <int> inter1, inter2;
+    vector <int> solution;
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-         vector<int> ans;
-    unordered_set<int> set{begin(nums1), end(nums1)};
-
-    for (const int num : nums2)
-      if (set.erase(num))
-        ans.push_back(num);
-
-    return ans;
+        for(int i = 0; i < nums1.size(); i++)
+            inter1.insert(nums1[i]);
+        for(int i = 0; i < nums2.size(); i++)
+            if(inter1.find(nums2[i]) != inter1.end())
+                inter2.insert(nums2[i]);//populate the intersection set
+        for(set<int>::iterator it = inter2.begin(); it != inter2.end(); ++it)
         
+            solution.push_back(*it);//copy the set into a vector
+        return solution;
     }
 };
