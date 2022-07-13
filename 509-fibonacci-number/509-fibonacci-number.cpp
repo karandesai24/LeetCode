f679c74eb11bbc2f750c350beee2e210389e3e98
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-07-13 18:32:36 +0530

    Time: 14 ms (32.84%), Space: 6 MB (16.57%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int fib(int N) {
-         if (N == 0) return 0;
+         if (N == 0)
+         {
+             return 0;
+         }
+          if (N == 1)
+         {
+             return 1;
+         }
+        //int ans=fib(N-1)+fib(N-2);
+        return fib(N-1)+fib(N-2);
+    }
+};
+/*
         int a = 0, b = 1;
         for(int i=2; i<=N; i++){
             int c = a + b;
             a = b;
             b = c;
         } return b;  
-        
-    }
-};
+        */
