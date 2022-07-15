313c1b04028ab29123366b52731ac1b443d4e31d
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-07-15 21:23:22 +0530

    Update 1985-find-the-kth-largest-integer-in-the-array.cpp

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -9,3 +9,13 @@ public:
         return nums[k-1];
     }
 };
+
+
+/*
+string kthLargestNumber(vector<string>& nums, int k) {
+ nth_element(begin(nums), begin(nums) + k - 1, end(nums), [](const auto& a, const auto& b) {
+      return a.length() == b.length() ? a > b : a.length() > b.length();      
+    });
+    return nums[k - 1];
+    }
+    */
