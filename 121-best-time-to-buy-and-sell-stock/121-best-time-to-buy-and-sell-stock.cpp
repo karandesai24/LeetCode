12acac745386ce32209c23265ca3c31bf1496d62
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-07-15 19:34:20 +0530

    Update 121-best-time-to-buy-and-sell-stock.cpp

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -13,3 +13,17 @@ public:
         
     }
 };
+
+/*
+
+ int valley = INT_MAX;
+        int ans = 0;
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < valley){
+                valley = prices[i];
+            }else if(prices[i] - valley > ans){
+                ans = prices[i] - valley;
+            }
+        }
+        return ans;
+*/
