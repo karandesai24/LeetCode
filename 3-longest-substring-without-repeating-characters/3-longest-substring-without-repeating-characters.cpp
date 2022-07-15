0cad10af36884947a7fec003e3be1aecea81d51f
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-07-15 22:37:30 +0530

    Update 3-longest-substring-without-repeating-characters.cpp

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -15,3 +15,21 @@ public:
         
     }
 };
+
+/*
+  int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        vector<int> position(128, -1);
+        int ans = 0;
+        
+        for(int i = 0, j = 0; j < n; j++){
+            if(position[s[j]] != -1){
+                i = max(i, position[s[j]]+1);
+            }
+            ans = max(ans, j-i+1);
+            position[s[j]] = j;
+        }
+        
+        return ans;
+    }
+    */
