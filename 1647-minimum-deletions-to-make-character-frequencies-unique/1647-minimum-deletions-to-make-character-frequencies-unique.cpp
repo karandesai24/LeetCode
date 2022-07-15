51be466a4685f666ad3f7dcf94377531fb7d8542
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-07-15 20:52:12 +0530

    Update 1647-minimum-deletions-to-make-character-frequencies-unique.cpp

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -19,3 +19,22 @@ public:
         
     }
 };
+
+
+/*
+
+ int minDeletions(string s) {
+    vector<int> freq(26);
+    for (char c : s) ++freq[c - 'a'];    
+    unordered_set<int> seen;
+    int ans = 0;
+    for (int f : freq) {
+      while (f && !seen.insert(f).second) {        
+        --f;
+        ++ans;
+      }
+    }
+    return ans;
+  }
+  
+  */
