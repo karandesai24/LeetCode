a7083d3aec83c57c30055ce097570fd3721a40c2
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-07-15 21:03:27 +0530

    Update 1752-check-if-array-is-sorted-and-rotated.cpp

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -16,3 +16,14 @@ public:
         
     }
 };
+
+/*
+
+int count = 0;
+   for (int i = 0; i < n; i++) {
+      if (arr[i] > arr[(i + 1) % n])
+         count++;
+   }
+   return (count <= 1);
+   
+   */
