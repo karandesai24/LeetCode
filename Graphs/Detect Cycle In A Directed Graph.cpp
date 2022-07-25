ef8154ff7790d158b6bb665f4a74fad57680e979
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-07-25 22:14:04 +0530

    Update Detect Cycle In A Directed Graph.cpp

--- Graphs/Detect Cycle In A Directed Graph.cpp
+++ Graphs/Detect Cycle In A Directed Graph.cpp
@@ -40,3 +40,57 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
     return false;
     
 }
+
+#BFS
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+  unordered_map<int,vector<int>> adj;
+    for(int i=0;i<edges.size();i++){
+        int u=edges[i].first - 1;
+        int v=edges[i].second - 1;
+        
+        adj[u].push_back(v);//directed graph
+    }
+    //finding indegree
+    vector<int> indegree(n);
+    
+    for(auto i : adj){
+        for(auto j: i.second){
+            indegree[j]++;
+        }
+    }
+    
+    
+//puhing in queue
+    queue<int> q;
+    for(int i=0;i<n;i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+        
+        int cnt = 0;
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            
+            cnt++;
+            
+            //indegree uptade
+
+            
+            for(auto i:adj[front]){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    q.push(i);
+                }
+            }
+        }
+    if(cnt == n){
+        return false;
+        
+    }else{
+        return true;
+    }
+
+}
