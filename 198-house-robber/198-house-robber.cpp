ebca41ae96af22e1b5dca531f902d5deb574ba0d
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-07-30 21:59:51 +0530

    Update 198-house-robber.cpp

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -12,3 +12,20 @@ private:
                             rob_ornot(nums, i-1, dp));
     }
 };
+/*
+class Solution {
+ public:
+  int rob(vector<int>& nums) {
+    int prev1 = 0;  // dp[i - 1]
+    int prev2 = 0;  // dp[i - 2]
+
+    for (const int num : nums) {
+      const int dp = max(prev1, prev2 + num);
+      prev2 = prev1;
+      prev1 = dp;
+    }
+
+    return prev1;
+  }
+};
+*/
