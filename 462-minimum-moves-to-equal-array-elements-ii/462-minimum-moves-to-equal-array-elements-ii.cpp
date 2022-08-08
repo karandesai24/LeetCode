a4f5230b9e24c2fa42631b4c3eb5c2a79e5d6e10
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-08-09 00:31:40 +0530

    Update 462-minimum-moves-to-equal-array-elements-ii.cpp

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -9,3 +9,11 @@ public:
         
     }
 };
+/*
+  int minMoves2(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int median = nums[nums.size() / 2], res = 0;
+        for(int i=0; i<nums.size(); i++){
+            res += abs(nums[i] - median);
+        } return res;
+        */
