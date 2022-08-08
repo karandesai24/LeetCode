4c9bc7f5077bf6d4f4858a504465af115b9dfb93
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-08-09 00:19:01 +0530

    Update 628-maximum-product-of-three-numbers.cpp

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -30,3 +30,13 @@ public:
         
     }
 };
+/*
+ int maximumProduct(vector<int>& nums) {
+       sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return (nums[n-1] * nums[n-2] * nums[n-3])
+                 > (nums[0] * nums[1] * nums[n-1]) ? 
+               (nums[n-1] * nums[n-2] * nums[n-3]) 
+                 : (nums[0] * nums[1] * nums[n-1]);
+    }
+    */
