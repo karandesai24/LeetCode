754deac5440a845babe759e05b7b1cc8df81311d
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-08-08 21:40:51 +0530

    Time: 4 ms (24.72%), Space: 6.1 MB (11.67%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> arr(n+1, 0);
+        arr[0] = 1, arr[1] = 1;
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<=i; ++j) 
+                arr[i] += arr[j-1] * arr[i-j];
+        } 
+        return arr[n];
+        
+    }
+};
