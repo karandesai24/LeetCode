e0cc122636c28072a198f330382ac7841ac3a798
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-08-11 00:28:11 +0530

    Time: 10 ms (55.88%), Space: 10.6 MB (46.13%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-         string ret, tmp;
-        vector<string> stack;
-        for ( istringstream iss(path); getline(iss, tmp, '/');  ) {
-            if (tmp == ".." && !stack.empty()) 
-                stack.pop_back();
-            else if (tmp == "." || tmp == ".." || tmp == "")
-                continue;
-            else stack.push_back(tmp);
-        }
-        for (auto str : stack)
-        { ret += "/" + str; }
-        return ret.empty() ? "/" : ret;
+         
+        stack<string> st;
+        string res;
         
+        for(int i = 0;  i<path.size(); ++i)
+        {
+            if(path[i] == '/')    
+                continue;
+            string temp;
+			// iterate till we doesn't traverse the whole string and doesn't encounter the last /
+            while(i < path.size() && path[i] != '/')
+            {
+				// add path to temp string
+                temp += path[i];
+                ++i;
+            }
+            if(temp == ".")
+                continue;
+			// pop the top element from stack if exists
+            else if(temp == "..")
+            {
+                if(!st.empty())
+                    st.pop();
+            }
+            else
+			// push the directory file name to stack
+                st.push(temp);
+        }
+        
+		// adding all the stack elements to res
+        while(!st.empty())
+        {
+            res = "/" + st.top() + res;
+            st.pop();
+        }
+        
+		// if no directory or file is present
+        if(res.size() == 0)
+            return "/";
+        
+        return res;
     }
 };
