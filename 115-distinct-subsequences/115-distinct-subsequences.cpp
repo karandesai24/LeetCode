b8bfa0f1411a7a5bfa69cc4abfb527ec02dd638b
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-09-08 11:37:53 +0530

    Update 115-distinct-subsequences.cpp

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -26,3 +26,29 @@ public:
 	return dp[n][m];  
     }
 };
+/*
+class Solution {
+public:
+	int solve(string& s,string& t,int i,int j,vector<vector<int>>& dp){
+		if(i>=s.length()&&j<t.length()){
+			return 0;
+		}
+		if(j>=t.length())return 1;
+		if(dp[i][j]!=-1)return dp[i][j];
+		int ans = 0;
+		if(s[i]==t[j]){
+		// there are 2 possibilities 1. take it 2. not take it
+			ans += solve(s,t,i+1,j+1,dp); 
+			ans += solve(s,t,i+1,j,dp);
+		}
+		else{
+			ans += solve(s,t,i+1,j,dp);
+		}
+		return dp[i][j] = ans;
+	}
+	int numDistinct(string s, string t) {
+		vector<vector<int>> dp(s.length()+1,vector<int>(t.length()+1,-1));
+		return solve(s,t,0,0,dp);
+	}
+};
+*/
