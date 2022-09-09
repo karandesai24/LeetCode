9677a34400380362df60ae6254cde3750e06a0fe
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-09-09 18:04:44 +0530

    Update 646-maximum-length-of-pair-chain.cpp

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -20,3 +20,24 @@ public:
         return solve(0,-1,pairs.size(),pairs,dp);
     }
 };
+/*
+class Solution {
+public:
+    static bool cmp (vector<int> a, vector<int> b) {
+        return a[1] < b[1];
+    }
+    
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), cmp);
+        int res = 0;
+        int last = -1002;
+        for (auto& v : pairs) {
+            if (last < v[0]) {
+                last = v[1];
+                res++;
+            }
+        }
+        return res;
+    }
+};
+*/
