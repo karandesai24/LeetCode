d37772e5bea2cde497bc2621f42a6aa60cbb9b54
Karan Desai <78134745+karandesai24@users.noreply.github.com>
2022-09-30 23:54:39 +0530

    Update 560-subarray-sum-equals-k.cpp

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -16,3 +16,17 @@ public:
     return ans;
     }
 };
+int subarraySum(vector<int>& nums, int k) {
+        int sum=0;
+        int count=0;
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum==k)
+                count++;
+            if(m.find(sum-k)!=m.end())
+                count+=m[sum-k];
+            m[sum]++;
+        }
+        return count;
+    }
